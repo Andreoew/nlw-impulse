7583b0fa8384da12b5d92131a169916085ad297b
Andreoew <57296980+Andreoew@users.noreply.github.com>
2022-05-08 04:56:22 -0300

    update server

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,9 +9,6 @@ app.use(express.json());
 app.use(routes);
 
 
-
-
-
-app.listen(3333, () => {
-  console.log('HTTP server running!');
-});
+app.listen(process.env.PORT || 3333, () => {
+  console.log('HTTP server listening on port 3333')
+})
